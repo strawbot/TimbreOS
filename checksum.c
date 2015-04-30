c223d3566bb2bafb72d9a2fbefb628651939dd4a
Robert Chapman III <rc@interbaun.com>
2015-04-30 08:20:52 -0600

    added code for standard fleschter 16, 32

--- checksum.c
+++ checksum.c
@@ -39,6 +39,44 @@ Short fletcher16(Byte *data, Long len)
 	return ((Short)sum2<<8) | (Short)sum1;
 }
 
+Short fletcher16Standard( Byte const *data, Long bytes )
+{
+        Short sum1 = 0xff, sum2 = 0xff;
+ 
+        while (bytes) {
+                size_t tlen = bytes > 20 ? 20 : bytes;
+                bytes -= tlen;
+                do {
+                        sum2 += sum1 += *data++;
+                } while (--tlen);
+                sum1 = (sum1 & 0xff) + (sum1 >> 8);
+                sum2 = (sum2 & 0xff) + (sum2 >> 8);
+        }
+        /* Second reduction step to reduce sums to 8 bits */
+        sum1 = (sum1 & 0xff) + (sum1 >> 8);
+        sum2 = (sum2 & 0xff) + (sum2 >> 8);
+        return sum2 << 8 | sum1;
+}
+
+Long fletcher32Standard( Short const *data, Long words )
+{
+        Long sum1 = 0xffff, sum2 = 0xffff;
+ 
+        while (words) {
+                unsigned tlen = words > 359 ? 359 : words;
+                words -= tlen;
+                do {
+                        sum2 += sum1 += *data++;
+                } while (--tlen);
+                sum1 = (sum1 & 0xffff) + (sum1 >> 16);
+                sum2 = (sum2 & 0xffff) + (sum2 >> 16);
+        }
+        /* Second reduction step to reduce sums to 16 bits */
+        sum1 = (sum1 & 0xffff) + (sum1 >> 16);
+        sum2 = (sum2 & 0xffff) + (sum2 >> 16);
+        return sum2 << 16 | sum1;
+}
+
 // 8 bit crc: X8 + X5 + X4 + 1 => 0x31 or flipped is: 0x8C
 // This table comes from Dallas sample code where it is freely reusable,
 // though Copyright (C) 2000 Dallas Semiconductor Corporation
