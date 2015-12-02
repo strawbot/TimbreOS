fa06a728dcd96f92ef0c31f5bd2291c34543f136
Robert Chapman III <rc@interbaun.com>
2015-12-02 16:10:20 -0700

    CRC32 additions

--- checksum.c
+++ checksum.c
@@ -148,3 +148,29 @@ Short crc16Slow(Short Crc, Byte Data) // slower, no table
 
 	return(Crc);
 }
+
+// ----------------------------- crc32b --------------------------------
+// http://stackoverflow.com/questions/21001659/crc32-algorithm-implementation-in-c-without-a-look-up-table-and-with-a-public-li
+/* This is the basic CRC-32 calculation with some optimization but no
+table lookup. The the byte reversal is avoided by shifting the crc reg
+right instead of left and by using a reversed 32-bit word to represent
+the polynomial.
+*/
+
+unsigned int crc32Slow(unsigned char *message) {
+   int i, j;
+   unsigned int byte, crc, mask;
+
+   i = 0;
+   crc = 0xFFFFFFFF;
+   while (message[i] != 0) {
+      byte = message[i];            // Get next byte.
+      crc = crc ^ byte;
+      for (j = 7; j >= 0; j--) {    // Do eight times.
+         mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xEDB88320 & mask);
+      }
+      i = i + 1;
+   }
+   return ~crc;
+}
