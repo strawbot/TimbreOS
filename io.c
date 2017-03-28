acd372cf009c54ece2447cdf4a157fc55075e522
Robert Chapman III <rc@interbaun.com>
2017-03-27 20:43:44 -0600

    fixed a typo in float words factored input and output separate from their machinery

--- io.c
+++ io.c
@@ -5,31 +5,39 @@
 #include "printers.h"
 #include "machines.h"
 
-// cli io
+﻿// platform specific io
 void output() {
     if (qbq(emitq) && (USART_StatusGet(UART1) & USART_STATUS_TXBL))
         USART_Tx(UART1, pullbq(emitq));
-    activate(output);
 }
 
 void input() {
     if (USART_StatusGet(UART1) & USART_STATUS_RXDATAV)
         pushbq(USART_RxDataGet(UART1), keyq);
-    activate(input);
 }
 
-void handleCli() {
+// machinery
+void outputMachine() {
+	output();
+	activate(outputMachine);
+}
+
+void inputMachine() {
+	input();
+    activate(inputMachine);
+}
+
+void cliMachine() {
     cli();
-    activate(handleCli);
-}
-
-void hello() {
-    print("hi");
+    activate(cliMachine);
 }
 
 void initIo() { // call to set up and run ios
+﻿	initMachines();
     resetCli();
-    activateOnce(input);
-    activateOnce(output);
-    activateOnce(handleCli);
+    print("Timbre CLI");
+    dotPrompt();
+    activateOnce(inputMachine);
+    activateOnce(outputMachine);
+    activateOnce(cliMachine);
 }
