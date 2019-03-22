232630f5c3096fef0d52b5f9d63b45c3da474edf
Robert Chapman III <rc@interbaun.com>
2019-03-22 00:26:54 -0600

    experimental TEA interface with optional embedded names

---
+++ next.hpp
@@ -0,0 +1,4 @@
+extern "C++" void next(void* object, unafun unary, const char * name=NOT_NAMED);
+extern "C++" void later(void* object, unafun unary, const char * name=NOT_NAMED);
+extern "C"   void next (vector machine);
+extern "C"   void later (vector machine);
